6506db13c6b4c81705218d0b082d99e60eb0146a
Sohan Bhadalkar <chimpbowl502@gmail.com>
2025-06-07 18:01:42 +0530

    snap added

--- client/src/components/landingPage/topsection.jsx
+++ client/src/components/landingPage/topsection.jsx
@@ -14,11 +14,11 @@ const TopSection=()=>{
 
     return(
         
-        <div className="flex items-center justify-between w-full h-[50px] z-[10] fixed pl-[5px] pr-[15px] backdrop-blur-[20px]">
-            <div data-label="logaContainer" className="flex min-w-[40px] h-[40px] bg-no-repeat bg-[url('/logo2.png')] bg-contain"></div>
+        <div className="flex items-center justify-between w-full h-[50px] z-[10] fixed pl-[15px] pr-[15px] backdrop-blur-[20px]">
+            <div data-label="logaContainer" className="flex min-w-[70px] h-[40px] bg-no-repeat bg-[url('/logoblacktext.png')] bg-contain"></div>
             <div data-label="SignupLoginRedirectDiv" className="flex w-fit h-[40px]">
                 <button className="w-[70px] text-white bg-black rounded mr-[10px] hover:translate-y-[5px] transition duration-300" onClick={loginRedirect}>Login</button>
-                <button className="w-[70px] text-black border-[2px] border-black rounded hover:translate-y-[5px] transition duration-300" onClick={registerRedirect}>Signup</button>
+                <button className="w-[70px] text-black bg-gray-200 border-[2px] border-black rounded hover:translate-y-[5px] transition duration-300" onClick={registerRedirect}>Signup</button>
             </div>
         </div>
     );
