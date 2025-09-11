2e6e355fa2982e1924d9264516f8b3b5d6fcb3fc
sohan <chimpbowl502@gmail.com>
2025-09-11 14:36:32 +0530

    responsive 3

--- client/src/components/home/contentSection/content.jsx
+++ client/src/components/home/contentSection/content.jsx
@@ -7,7 +7,7 @@ import Carousel from './carousel.jsx';
 
 const Content=()=>{
 return(
-<section className=" w-[calc(100%-70px)] pr-[35px] h-full ml-[70px] max-[985px]:pr-[15px]"> 
+<section className=" w-[calc(100%-70px)] max-[675px]:w-[100%] pr-[35px] h-full ml-[70px] max-[985px]:pr-[15px] max-[675px]:px-[20px] max-[675px]:ml-[0px]"> 
    <TopBar></TopBar>
    <div className="flex w-full min-h-[calc(100%-120px)] max-[985px]:flex-col">
       <div className="flex flex-col w-[calc(100%-250px)] h-fit px-[20px] max-[985px]:px-[0px] max-[985px]:w-full">   
