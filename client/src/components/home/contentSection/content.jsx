3e1a7ee6957a04adf57b05be4d7f12d6819ba18f
sohan <chimpbowl502@gmail.com>
2025-09-11 12:23:22 +0530

    right container collapse below 985px

--- client/src/components/home/contentSection/content.jsx
+++ client/src/components/home/contentSection/content.jsx
@@ -7,10 +7,10 @@ import Carousel from './carousel.jsx';
 
 const Content=()=>{
 return(
-<section className=" w-[calc(100%-70px)] pr-[35px] h-full ml-[70px] "> 
+<section className=" w-[calc(100%-70px)] pr-[35px] h-full ml-[70px] max-[985px]:pr-[15px]"> 
    <TopBar></TopBar>
-   <div className="flex w-full min-h-[calc(100%-120px)]">
-      <div className="flex flex-col w-[calc(100%-250px)] h-fit px-[20px]">   
+   <div className="flex w-full min-h-[calc(100%-120px)] max-[985px]:flex-col">
+      <div className="flex flex-col w-[calc(100%-250px)] h-fit px-[20px] max-[985px]:px-[0px] max-[985px]:w-full">   
         <MotivationDiv/>
         <TodaySchedule/>
         <Carousel/>
